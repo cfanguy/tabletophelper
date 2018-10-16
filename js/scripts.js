e245ad6c3225a5862188588ffde1ea9bdd919663
Chad Fanguy <cfanguy@gmail.com>
2018-10-15 22:46:08 -0400

    Player window draggable, added Monster button, and code cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
+// grid array for draggable object
 var arr = [10, 10];
 var plNum = 0;
 
+// add player to table
 function addPl(name, id, ac, hp) {
-    html = "<tr><td style='margin-right: 10px;'>" + name + "</td><td style='margin-right: 10px;'>" + id + "</td><td style='margin-right: 10px;'>" + ac + "</td><td><input type='text' value='" + hp + "'/></td></tr>";
+    html = "<tr><td>" +
+        name + "</td><td>" +
+        id + "</td><td>" +
+        ac + "</td><td><input type='text' value='" +
+        hp + "'/></td></tr>";
 
     document.getElementById("playerTbl").innerHTML += html;
     
     
-    htmlDraggables = "<div id='draggable" + plNum + "' class='box player'>" + id + "</div>";
+    htmlDraggables = "<div id='draggable" +
+        plNum + "' class='box player'>" +
+        id + "</div>";
     document.getElementById("playersDiv").innerHTML += htmlDraggables;
     
     $('#name').val("");
@@ -19,22 +27,35 @@ function addPl(name, id, ac, hp) {
 }
 
 
+// hide modal window and initialize draggables
 function hideAndInitObjs() {
-    document.getElementById('modal').style.display = 'none';
-    
-    for(var i = 0; i < plNum; i++) {
-        $("#draggable" + i).draggable({ grid: arr })
+    document.getElementById('playerModal').style.display = 'none';
+
+    $("div[id^='draggable'], .scenery, #playerAndHP").draggable({
+        grid: arr
+    });
+}
+
+
+function addMonster() {
+    document.getElementById('monsterModal').style.display = '';
+}
+
+
+function createAndInitMonster() {
+    var monsterType = document.getElementById("monsterSelection").value;
+    var newMonster;
+
+    if(monsterType === "normal") {
+        newMonster = '<div class="box monster">New X</div>';
     }
+    else {
+        newMonster = '<div class="monster ' + monsterType + '">new ' + monsterType + '</div>';
+    }
+    document.getElementById("monsters").innerHTML += newMonster;
+    $(".monster").draggable({
+        grid: arr
+    });
 
-    $("#mon1, #mon2, #mon3, #mon4, #mon5, #mon6, #mon7, #mon8").draggable({ grid: arr })
-
-    $("#scenb1, #scenb2, #scenb3, #scenb4, #scenb5, #scenb6, #scenb7, #scenb8, #scenb9, #scenb10, #scenb11, #scenb12, #scenb13, #scenb14, #scenb15").draggable({ grid: arr })
-
-    $("#scend1, #scend2, #scend3, #scend4, #scend5").draggable({ grid: arr })
-
-    $("#sceng1, #sceng2, #sceng3, #sceng4, #sceng5, #sceng6, #sceng7, #sceng8, #sceng9, #sceng10").draggable({ grid: arr })
-
-    $("#scenw1, #scenw2, #scenw3, #scenw4, #scenw5, #scenw6, #scenw7, #scenw8, #scenw9, #scenw10").draggable({ grid: arr })
-
-    $("#largeMonster, #hugeMonster, #gargMonster, #colMonster").draggable({ grid: arr })
+    document.getElementById('monsterModal').style.display = 'none';
 }
